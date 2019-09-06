6233055e3bdbcb500735ab6834c748e136ec18b4
Nicklas Nordenmark <nicklas.nordenmark@freespee.com>
2019-09-06 14:24:54 +0200

    Fixes merge, started work on modalBottomSheet

--- lib/components/players.component.dart
+++ lib/components/players.component.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:yaniv/components/score-form.component.dart';
 
 import 'package:yaniv/models/player.model.dart';
 
@@ -6,6 +7,17 @@ class PlayersComponent extends StatelessWidget {
   PlayersComponent({this.players});
 
   final List<Player> players;
+
+  void handleTappedPlayer(Player player, BuildContext context) {
+    debugPrint(player.name);
+
+    showModalBottomSheet(
+      context: context,
+      builder: (context) => Container(
+        child: Text(player.name),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,8 +31,12 @@ class PlayersComponent extends StatelessWidget {
         return Container(
           height: 50,
           child: ListTile(
+              leading: Text(player.points.toString()),
               title: Text(player.name),
-              trailing: Text(player.points.toString())),
+              trailing: Icon(Icons.keyboard_arrow_right),
+              onTap: () {
+                handleTappedPlayer(player, context);
+              }),
         );
       },
     );
