5b279bb94951b2b8348740512afd9452e5fe185b
Nicklas Nordenmark <nicklas.nordenmark@freespee.com>
2019-09-06 13:07:39 +0200

    List players with scores on GameScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:yaniv/screens/game.screen.dart';
 import 'package:yaniv/screens/games.screen.dart';
 
 void main() => runApp(MyApp());
@@ -21,6 +22,10 @@ class MyApp extends StatelessWidget {
           // is not restarted.
           primarySwatch: Colors.blue,
         ),
-        home: GamesScreen());
+        initialRoute: '/',
+        routes: {
+          '/': (context) => GamesScreen(),
+          '/game': (context) => GameScreen(),
+        });
   }
 }
--- lib/screens/game.screen.dart
+++ lib/screens/game.screen.dart
@@ -1,16 +1,28 @@
 import 'package:flutter/material.dart';
-import 'dart:developer' as developer;
+
+import 'package:yaniv/components/players.component.dart';
+import 'package:yaniv/models/game.model.dart';
+import 'package:yaniv/models/player.model.dart';
 
 class GameScreen extends StatelessWidget {
+  final Game game = Game.randomize();
+
+  void _sortPlayersByScore(List<Player> players) {
+    players.sort((Player p1, Player p2) => p2.score - p1.score);
+  }
+
   @override
   Widget build(BuildContext context) {
+    _sortPlayersByScore(this.game.players);
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Single game'),
+        title: Text("Game scores"),
       ),
+      body: PlayersComponent(players: this.game.players),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          developer.log('Add player');
+          debugPrint('Add player');
         },
         tooltip: 'Add player',
         child: Icon(Icons.add),
