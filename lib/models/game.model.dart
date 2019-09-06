3f9e590de1132e9041e33631348e7b466b775078
Nicklas Nordenmark <nicklas.nordenmark@freespee.com>
2019-09-06 10:08:31 +0200

    Adds models for Game and Player and a way of constructing random data for now

--- lib/models/game.model.dart
+++ lib/models/game.model.dart
@@ -1,3 +1,4 @@
+import 'package:faker/faker.dart';
 import 'package:yaniv/models/player.model.dart';
 
 class Game {
@@ -5,4 +6,18 @@ class Game {
   List<Player> players;
   bool completed;
   int createdAt;
+
+  Game.fromJSON(Map<String, dynamic> json) {
+    id = json['id'];
+    players = json['players'].map((playerJson) => Player.fromJSON(playerJson));
+    completed = json['completed'];
+    createdAt = json['createdAt'];
+  }
+
+  Game.randomize() {
+    id = faker.guid.guid();
+    players = List.generate(random.integer(8), (n) => Player.randomize());
+    completed = random.boolean();
+    createdAt = DateTime.now().millisecondsSinceEpoch;
+  }
 }
--- lib/screens/games.screen.dart
+++ lib/screens/games.screen.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
 import 'dart:developer' as developer;
 
+import 'package:yaniv/models/game.model.dart';
+
 class GamesScreen extends StatelessWidget {
+  final List<Game> games = [];
+
+  GamesScreen() {
+    for (var i = 0; i < 10; i++) {
+      games.add(Game.randomize());
+      developer
+          .log("Added: id: ${games[i].id} players: ${games[i].players.length}");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
