fbbb54b77766a8cddc7f752f32331bf421694f06
ThalapathySiva <sivaramsiva16@gmail.com>
2019-10-22 21:03:10 +0530

    Activity Indicator When logging in

--- lib/screens/login.screen.dart
+++ lib/screens/login.screen.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:google_sign_in/google_sign_in.dart';
@@ -13,8 +14,22 @@ class LoginState extends State<LoginScreen> {
   final GoogleSignIn _googleSignIn = GoogleSignIn(scopes: ['email']);
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseService firebaseService = FirebaseService();
+  bool showIndicator = false; 
 
+  @override
+  build(BuildContext context) => Scaffold(
+        body: showIndicator
+            ? Center(child: CupertinoActivityIndicator())
+            : new Center(
+                child: new RaisedButton(
+                child: new Text('Sign in with google'),
+                onPressed: () => _handleLogin(context),
+              )),
+      );
   _handleLogin(BuildContext context) async {
+    setState(() {
+      showIndicator = true;
+    });
     final GoogleSignInAccount googleUser = await _googleSignIn.signIn();
     final GoogleSignInAuthentication googleAuth =
         await googleUser.authentication;
@@ -25,20 +40,20 @@ class LoginState extends State<LoginScreen> {
     );
     try {
       FirebaseUser user = (await _auth.signInWithCredential(credential)).user;
+      print("user" + user.toString());
+
+      setState(() {
+        showIndicator = false;
+      });
 
       firebaseService.setEmail(user.email);
       Navigator.pushNamed(context, '/games');
     } catch (e) {
+      print("fsf");
+      setState(() {
+        showIndicator = false;
+      });
       debugPrint(e.toString());
     }
   }
-
-  @override
-  build(BuildContext context) => Scaffold(
-        body: new Center(
-            child: new RaisedButton(
-          child: new Text('Sign in with google'),
-          onPressed: () => _handleLogin(context),
-        )),
-      );
 }
