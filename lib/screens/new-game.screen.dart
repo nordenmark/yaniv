c0740d45b29cf71659e96244665e32be0e83d3a7
Damoon Rashidi <damoon.rashidi@gmail.com>
2020-03-13 14:37:34 +0100

    choose game limit in create game screen

--- lib/screens/new-game.screen.dart
+++ lib/screens/new-game.screen.dart
@@ -14,6 +14,8 @@ class NewGameScreenState extends State<NewGameScreen> {
   final _nameController = TextEditingController(text: '');
 
   final _headingStyle = TextStyle(fontSize: 12, letterSpacing: 1.2);
+  int _gameLength = 201;
+  List<int> lengthOptions = [101, 151, 201];
 
   String _isNotEmpty(String value) {
     if (value.isEmpty) {
@@ -22,9 +24,16 @@ class NewGameScreenState extends State<NewGameScreen> {
     return null;
   }
 
+  setGameLength(int length) {
+    setState(() {
+      _gameLength = length;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: Colors.grey[200],
         body: Padding(
             padding: EdgeInsets.all(64),
             child: Form(
@@ -47,15 +56,25 @@ class NewGameScreenState extends State<NewGameScreen> {
                         controller: _nameController,
                         validator: _isNotEmpty,
                       ),
-                      new Text('GAME LENGTH', style: _headingStyle),
-                      new Container(
+                      Text('GAME LENGTH', style: _headingStyle),
+                      Expanded(
+                          child: ListView(
+                              children: List.generate(
+                                  lengthOptions.length,
+                                  (index) => RadioWrapper(
+                                        value: lengthOptions[index],
+                                        groupValue: _gameLength,
+                                        onPressed: (value) =>
+                                            setGameLength(value),
+                                      )))),
+                      Container(
                           width: 300,
                           height: 40,
                           child: new PillButton(
                             onPressed: () async {
                               if (_formKey.currentState.validate()) {
                                 var id = await _firebaseService.createNewGame(
-                                    gameLength: 201,
+                                    gameLength: _gameLength,
                                     name: _nameController.value.text);
                                 Navigator.push(
                                     context,
@@ -73,5 +92,44 @@ class NewGameScreenState extends State<NewGameScreen> {
                                 colors: [Colors.blue[300], Colors.blue[500]]),
                           ))
                     ]))));
+  }
+}
+
+class RadioWrapper extends StatelessWidget {
+  final Function onPressed;
+  final int value;
+  final int groupValue;
+
+  RadioWrapper({this.value, this.onPressed, this.groupValue});
+
+  @override
+  build(BuildContext context) {
+    return Card(
+        elevation: 8,
+        child: Padding(
+          padding: EdgeInsets.all(16),
+          child: Row(
+            children: [
+              new Expanded(
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                    Text("Scoring limit of $value",
+                        style: TextStyle(fontSize: 14)),
+                    Text('Game ends when a player reaches $value points',
+                        style: TextStyle(fontSize: 8))
+                  ])),
+              new Radio(
+                value: value,
+                groupValue: groupValue,
+                activeColor: Colors.blue,
+                onChanged: (value) {
+                  onPressed(value);
+                },
+              )
+            ],
+          ),
+        ));
   }
 }
